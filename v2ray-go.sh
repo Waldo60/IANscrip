ea9163a62445f9c7e78d78932d43e3301559ecf7
Waldo60 <80797901+Waldo60@users.noreply.github.com>
2024-05-12 23:04:05 -0400

    Update v2ray-go.sh

--- v2ray-go.sh
+++ v2ray-go.sh
@@ -7,27 +7,27 @@ NC='\e[0m'
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[information]${Font_color_suffix}"
 MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-CEKEXPIRED () {
-    today=$(date -d +1day +%Y-%m-%d)
-    Exp1=$(curl -sS https://raw.githubusercontent.com/Iansoftware/userip/main/bossip | grep $MYIP | awk '{print $3}')
-    if [[ $today < $Exp1 ]]; then
-    echo -e "\e[32mSTATUS SCRIPT AKTIF...\e[0m"
-    else
-    echo -e "\e[31mSCRIPT ANDA EXPIRED!\e[0m";
-    echo -e "\e[31mRenew IP letak tempoh banyak kit okay? hehe syg ktk #\e[0m"
-    exit 0
-fi
-}
-IZIN=$(curl -sS https://raw.githubusercontent.com/Iansoftware/userip/main/bossip | awk '{print $4}' | grep $MYIP)
-if [ $MYIP = $IZIN ]; then
-echo -e "\e[32mPermission Accepted...\e[0m"
-CEKEXPIRED
-else
-echo -e "\e[31mPermission Denied!\e[0m";
-echo -e "\e[31mDaftar IP dalam github lok sayang okay? mun dah daftar tapi masih juak permission denied refresh dolok website ya hehe. Love you #\e[0m"
-exit 0
-fi
+#echo "Checking VPS"
+#CEKEXPIRED () {
+#    today=$(date -d +1day +%Y-%m-%d)
+#    Exp1=$(curl -sS https://raw.githubusercontent.com/Iansoftware/userip/main/bossip | grep $MYIP | awk '{print $3}')
+#    if [[ $today < $Exp1 ]]; then
+#    echo -e "\e[32mSTATUS SCRIPT AKTIF...\e[0m"
+#    else
+#    echo -e "\e[31mSCRIPT ANDA EXPIRED!\e[0m";
+#    echo -e "\e[31mRenew IP letak tempoh banyak kit okay? hehe syg ktk #\e[0m"
+#    exit 0
+#fi
+#}
+#IZIN=$(curl -sS https://raw.githubusercontent.com/Iansoftware/userip/main/bossip | awk '{print $4}' | grep $MYIP)
+#if [ $MYIP = $IZIN ]; then
+#echo -e "\e[32mPermission Accepted...\e[0m"
+#CEKEXPIRED
+#else
+#echo -e "\e[31mPermission Denied!\e[0m";
+#echo -e "\e[31mDaftar IP dalam github lok sayang okay? mun dah daftar tapi masih juak permission denied refresh dolok website ya hehe. Love you #\e[0m"
+#exit 0
+#fi
 clear
 echo -e "${Info} V2ray CORE VPS AutoScript by IanVPN"
 # Detect public IPv4 address and pre-fill for the user
